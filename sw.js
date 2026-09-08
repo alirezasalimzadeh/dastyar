e864fb9609da97f6d78c55930e47dfd682a8f909
Alireza Salimzadeh <alireza.salim021@gmail.com>
2026-09-09 02:21:53 +0330

    Updates

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'dastyar-shell-v1';
+const baseUrl = new URL('./', self.registration.scope);
+const appUrl = (path = '') => new URL(path, baseUrl).href;
+const APP_SHELL = [appUrl(), appUrl('index.html'), appUrl('manifest.webmanifest'), appUrl('icon-192.png'), appUrl('icon-512.png')];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(fetch(request).catch(() => caches.match(appUrl('index.html'))));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      if (response.ok) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+      }
+      return response;
+    })),
+  );
+});
